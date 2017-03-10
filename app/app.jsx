00d39c6b4691f4b2fe24706b2ea707bf9298e635
kervin5 <kervinlvh@gmail.com>
2017-03-09 22:57:04 -0800

    Simple template for react apps

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Greeter = require('Greeter');
-
-var firstName = "Kervin";
 
 ReactDOM.render(
-    <Greeter name={firstName}/>,
+  <h1>Boilter plate app</h1>,
     document.getElementById('app')
   );
